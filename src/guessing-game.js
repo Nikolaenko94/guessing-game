4b812a1c371d2745bafd64fb2568f1465c3f358a
Stanislav Nikolaenko <grot_cs5@mail.ru>
2019-10-05 21:30:09 +0300

    task done

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,23 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() { }
+    
     setRange(min, max) {
-
+            this.min=min;
+            this.max=max;
     }
 
     guess() {
-
+        this.sum=this.max+this.min;
+        this.result =Math.round(this.sum/2);
+        return Math.round(this.result);
     }
 
     lower() {
-
+      return this.max=this.result;
     }
 
     greater() {
-
+        return this.min=this.result;
     }
 }
 
